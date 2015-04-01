2ceb7ea873ade64e70f213b02651ec51a17c67e8
sharmava <sharmava@indiana.edu>
2015-03-31 21:44:44 -0400

    Added todo.html

--- meanPrototype1/app/routes.js
+++ meanPrototype1/app/routes.js
@@ -57,6 +57,6 @@ module.exports = function(app) {
 
 	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		res.sendfile('./public/tripster.html'); // load the single view file (angular will handle the page changes on the front-end)
 	});
 };
